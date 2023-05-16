eb51989621158987646e2b9aec138a8f2d86150f
Tony Dantona <tonydantona@gmail.com>
2023-05-16 18:47:13 -0400

    init commit w/ day 2

---
+++ day2_TypesAndMathOps.py
@@ -0,0 +1,29 @@
+print(type('hello')) #? <class 'str'>
+print(type(123))   #? <class 'int'>
+
+a = float(123)
+print(type(a))    #? <class 'float'>
+
+print(70 + float('100.5')) #? 170.5
+
+print(str(70) + str(10)) #? 70100
+
+# two_digit_number = input("Type a two digit number: ")
+# first = two_digit_number[0]
+# second = two_digit_number[1]
+# print(int(first) + int(second))
+
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+print(2**2) #? exponent
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+print(float(height))
+#ans = float(weight) / float(height**2)
+
+#print(int(ans))
+
